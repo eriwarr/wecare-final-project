2153fb39c892be1ac39a7c7f9d814d933d7788ff
Eric Warren II <ericwarrenii@Erics-MacBook-Pro.local>
2021-07-22 10:08:30 -0400

    update frontend urls

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'frontend'
 
 urlpatterns = [
-    path('/<path:resource>/', views.IndexView.as_view()),
+    # path('/<path:resource>/', views.IndexView.as_view()),
     path('', views.IndexView.as_view(), name='index'),
 
 ]
